a2315c628bbf734f0cca5e21d75ff33257c99cd9
Kodemapa <kodemapa.tutor@gmail.com>
2024-02-15 16:04:39 +0530

    angularjs

---
+++ index.js
@@ -0,0 +1,15 @@
+var app=angular.module("app",[])
+
+app.controller("ConterollerName",function($scope){
+    //array of strings 
+    $scope.nav=[
+    {
+        href:"index.html",
+        tetx:"Home",
+    },
+] 
+
+$scope.pageInfo = "" //read the json and convert it into list of objects
+$scope.hero =$scope.pageInfo.hero
+$scope.
+});
---
+++ service.js
@@ -0,0 +1,2 @@
+var app=angular.module("app",[])
+app.controller("custs", ["$scope", function ($scope) {
