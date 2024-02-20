b5b03ff5cccee298f46f7bbf45a989e058b41c64
kodemapa <kodemapa.tutor@gmail.com>
2024-02-20 17:21:07 +0530

    angular code

--- js/index.js
+++ js/index.js
@@ -1,37 +1,31 @@
-var app=angular.module("app",[])
+var app = angular.module("app", []);
 
-app.controller("ControllerName",function($scope){
-    //array of strings 
-    const fs = require('fs');
-    var jsonObject;
-// Read the JSON file
-fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
-    }
+app.controller("ControllerName", function ($scope, $http) {
+    // Initialize $scope.pageInfo
+    $scope.pageInfo = {};
 
-    try {
-        // Parse the JSON data into a JavaScript object
-         jsonObject = JSON.parse(data);
+    // Use $http service to load JSON data
+    $http.get('data.json').then(function (response) {
+        // Handle successful response
+        $scope.pageInfo = response.data;
+        // console.log("json", $scope.pageInfo);
+        // Access the hero object
+        $scope.nav=$scope.pageInfo.nav;
+        $scope.hero = $scope.pageInfo.hero; 
+        $scope.AboutUs=$scope.pageInfo.AboutUs;
+        $scope.services=$scope.pageInfo.services; 
+        $scope.whyChooseUs=$scope.pageInfo.whyChooseUs;
+        $scope.OurProjects=$scope.pageInfo.OurProjects;
+        if(OurProjects.length<8)
+        {
+            $scope.OurProjects=$scope.pageInfo.OurProjects.slice(0,length);
+        }else{
 
-        // You can now use the jsonObject object in your code
-        console.log(jsonObject);
-    } catch (error) {
-        console.error('Error parsing JSON:', error);
-    }
-});
-
-
-    $scope.nav=[
-    {
-        href:"index.html",
-        text:"Home",
-    },
-] 
-
-$scope.pageInfo = jsonObject //read the json and convert it into list of objects
-console.log("json",$scope.pageInfo)
-$scope.hero =$scope.pageInfo.hero
-
+            $scope.OurProjects=$scope.pageInfo.OurProjects.slice(0,8);
+        }
+              
+    }, function (error) {
+        // Handle error
+        console.error('Error loading JSON:', error);
+    });
 });
