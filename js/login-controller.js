297d5a836c949b6068e4cd2b9bfb81706ea7e5d4
Kodemapa <kodemapa.tutor@gmail.com>
2024-04-11 12:45:52 +0530

    login/register

---
+++ js/login-controller.js
@@ -0,0 +1,38 @@
+var app = angular.module("app", []);
+
+app.controller("loginController", function ($scope, $http) {
+  $scope.loginInfo = {};
+  $scope.PasswordError = false;
+  $scope.login_info_entered = {
+    Email: "",
+    Password: "",
+  };
+
+  $scope.Submit = function () {
+    $http.get("../../login.json").then((response) => {
+      var userData = response.data; // Make sure this path is correct
+      var userFound = userData.users.some(
+        (user) =>
+          user.Email === $scope.login_info_entered.Email &&
+          user.Password === $scope.login_info_entered.Password
+      );
+
+      if (userFound) {
+        creatingUserObj(userData);
+        $scope.PasswordError = false;
+        window.location.href = "dashboard.html"; // Changed to redirect in the same window/tab
+      } else {
+        $scope.PasswordError = true;
+      }
+    });
+  };
+
+  var creatingUserObj = function (userData) {
+    let users = userData.users.filter(
+      (user) =>
+        user.Email === $scope.login_info_entered.Email &&
+        user.Password === $scope.login_info_entered.Password
+    );
+    localStorage.setItem("loggedInUser", JSON.stringify(users[0]));
+  };
+});
